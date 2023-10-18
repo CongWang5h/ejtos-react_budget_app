9018a9fc5448b9fe3db44bc8e89635f6c3a4d4fb
CongWang5h <congwang.5h@gmail.com>
2023-10-18 14:15:22 -0400

    add Currency

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,14 +2,29 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const inputValue = parseInt(event.target.value, 10);
+        setNewBudget(inputValue);
+
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total += item.cost);
+        }, 0);
+
+        if (inputValue >= 20000) {
+            alert("Cannot increase the allocation! Out of funds");
+        }
+
+        if (inputValue < totalExpenses) {
+            alert("You cannot reduce the budget below the remaining expenses");
+        }
     }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}</span>
             <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
         </div>
     );
